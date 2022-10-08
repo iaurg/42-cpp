7e22273c5aa88f79fd9e29d85df1f6a4ee347b78
Italo A <italoaurelior@gmail.com>
2022-10-08 12:23:08 -0300

    feat: create contact functions

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,33 +1,38 @@
 #include "Contact.hpp"
 
-/*
-** ------------------------------- CONSTRUCTOR --------------------------------
-*/
-
 Contact::Contact()
 {
-    std::cout << "Contact created" << std::endl;
 }
-
-Contact::Contact(const Contact& src)
-{
-}
-
-/*
-** -------------------------------- DESTRUCTOR --------------------------------
-*/
 
 Contact::~Contact()
 {
-    std::cout << "Contact destructor called" << std::endl;
 }
 
-/*
-** --------------------------------- METHODS ----------------------------------
-*/
+std::string Contact::getValue(std::string fieldName) const
+{
+    if (fieldName == "firstName")
+        return this->_firstName;
+    else if (fieldName == "lastName")
+        return this->_lastName;
+    else if (fieldName == "nickname")
+        return this->_nickname;
+    else if (fieldName == "phoneNumber")
+        return this->_phoneNumber;
+    else if (fieldName == "darkestSecret")
+        return this->_darkestSecret;
+    return "";
+}
 
-/*
-** --------------------------------- ACCESSOR ---------------------------------
-*/
-
-/* ************************************************************************** */
+void Contact::setValue(std::string fieldName, std::string& field)
+{
+    if (fieldName == "First name")
+        this->_firstName = field;
+    else if (fieldName == "Last name")
+        this->_lastName = field;
+    else if (fieldName == "Nickname")
+        this->_nickname = field;
+    else if (fieldName == "Phone number")
+        this->_phoneNumber = field;
+    else if (fieldName == "Darkest secret")
+        this->_darkestSecret = field;
+}
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -8,10 +8,16 @@ class Contact {
 
 public:
     Contact();
-    Contact(Contact const& src);
     ~Contact();
+    std::string getValue(std::string fieldName) const;
+    void setValue(std::string fieldName, std::string& field);
 
 private:
+    std::string _firstName;
+    std::string _lastName;
+    std::string _nickname;
+    std::string _phoneNumber;
+    std::string _darkestSecret;
 };
 
 #endif /* ********************************************************* CONTACT_H */
