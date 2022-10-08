f3078b2e49dc142b298e7dea847af267058c9f46
Italo A <italoaurelior@gmail.com>
2022-10-08 16:17:38 -0300

    fix: remove not allowed algorithm

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -1,19 +1,17 @@
-#include <iostream>
 #include <cstring>
-#include <algorithm>
+#include <iostream>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int i = 1;
-    if (argc == 1)
-    {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    while (argc > i)
-    {
-        std::transform(argv[i], argv[i] + strlen(argv[i]), argv[i], toupper);
-        std::cout << argv[i];
+    while (i < argc) {
+        for (size_t j = 0; j < std::strlen(argv[i]); j++) {
+            std::cout << (char)std::toupper(argv[i][j]);
+        }
         i++;
     }
     std::cout << std::endl;
