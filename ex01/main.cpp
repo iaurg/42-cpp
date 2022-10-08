47089c28c1b9a39e6809b2d0e8aa45f46f177041
Italo A <italoaurelior@gmail.com>
2022-10-08 12:23:29 -0300

    feat: add entry point

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,19 +3,7 @@
 int main(void)
 {
     PhoneBook phoneBook;
-    std::string command;
 
-    while (1) {
-        std::cout << "Enter a command: ";
-        std::getline(std::cin, command);
-        if (command == "EXIT")
-            break;
-        else if (command == "ADD")
-            phoneBook.addContact();
-        else if (command == "SEARCH")
-            phoneBook.searchContact();
-        else
-            std::cout << "Invalid command" << std::endl;
-    }
+    phoneBook.create();
     return (0);
 }
