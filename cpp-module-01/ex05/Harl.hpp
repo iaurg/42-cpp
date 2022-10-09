0ad788682095fc82c2ebae01032607494d2ab51c
Italo A <italoaurelior@gmail.com>
2022-10-09 18:37:09 -0300

    feat: start harl ex05

---
+++ cpp-module-01/ex05/Harl.hpp
@@ -0,0 +1,16 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl {
+
+public:
+    Harl();
+    ~Harl();
+
+private:
+};
+
+#endif /* ************************************************************ HARL_H */
