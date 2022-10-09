aa274a7db25dbd81ba048443b3aeaea5048f5112
Italo A <italoaurelior@gmail.com>
2022-10-09 20:39:50 -0300

    feat: start harl

--- cpp-module-01/ex05/Harl.cpp
+++ cpp-module-01/ex05/Harl.cpp
@@ -20,6 +20,30 @@ Harl::~Harl()
 ** --------------------------------- METHODS ----------------------------------
 */
 
+void Harl::complain(std::string level)
+{
+}
+
+void Harl::_debug(void)
+{
+    std::cout << "DEBUG: A debug error" << std::endl;
+}
+
+void Harl::_info(void)
+{
+    std::cout << "INFO: An info error" << std::endl;
+}
+
+void Harl::_warning(void)
+{
+    std::cout << "WARNING: A warning error" << std::endl;
+}
+
+void Harl::_error(void)
+{
+    std::cout << "ERROR: An error" << std::endl;
+}
+
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
--- cpp-module-01/ex05/Harl.hpp
+++ cpp-module-01/ex05/Harl.hpp
@@ -9,8 +9,13 @@ class Harl {
 public:
     Harl();
     ~Harl();
+    void complain(std::string level);
 
 private:
+    void _debug(void);
+    void _info(void);
+    void _warning(void);
+    void _error(void);
 };
 
 #endif /* ************************************************************ HARL_H */
