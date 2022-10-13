f7e37ac07b6c091874d50ec6946eaad6abda6edd
Italo A <italoaurelior@gmail.com>
2022-10-13 05:37:32 -0300

    refactor: remove unecessary overload

--- cpp-module-03/ex03/ClapTrap.hpp
+++ cpp-module-03/ex03/ClapTrap.hpp
@@ -41,6 +41,4 @@ protected:
     int _attackDamage;
 };
 
-std::ostream& operator<<(std::ostream& o, ClapTrap const& i);
-
 #endif /* ******************************************************** CLAPTRAP_H */
