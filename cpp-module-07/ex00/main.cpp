17a7fb37dd7f5da565b3a310ec3d3b485c539aff
Italo A <italoaurelior@gmail.com>
2022-10-26 22:08:35 -0300

    feat: testing template

---
+++ cpp-module-07/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "whatever.hpp"
+
+int main(void)
+{
+    Print(1);
+    Print(1.0f);
+    Print(1.0);
+    Print("1");
+    Print("1.0");
+    Print("Test");
+}
---
+++ cpp-module-07/ex00/whatever.hpp
@@ -0,0 +1,13 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void Print(T value)
+{
+    std::cout << value << std::endl;
+}
+
+#endif /* ************************************************************ WHATEVER_H */
