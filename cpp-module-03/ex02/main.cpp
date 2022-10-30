cd2600961ad243e650e0fc063a1cda11090a8a00
Italo A <italoaurelior@gmail.com>
2022-10-30 08:56:28 -0300

    fix: tests

--- cpp-module-03/ex02/main.cpp
+++ cpp-module-03/ex02/main.cpp
@@ -9,32 +9,27 @@ int main()
         ScavTrap sit("Sithy");
         FragTrap tim("Timothy");
 
-        std::cout << rob << std::endl;
-        std::cout << sit << std::endl;
-        std::cout << tim << std::endl;
+        std::cout << rob.getName() << std::endl;
+        std::cout << sit.getName() << std::endl;
+        std::cout << tim.getName() << std::endl;
     }
+
     std::cout << std::endl
               << std::endl;
     {
         FragTrap norb("Norbert");
 
         norb.attack("Jeremy");
-        std::cout << norb << std::endl;
         norb.takeDamage(25);
-        std::cout << norb << std::endl;
         norb.takeDamage(18);
-        std::cout << norb << std::endl;
         norb.attack("Valerie");
         norb.attack("Cassie");
         norb.attack("Victor");
         norb.attack("Tristan");
-        std::cout << norb << std::endl;
         norb.beRepaired(40);
-        std::cout << norb << std::endl;
         norb.attack("Killian");
         norb.highFivesGuys();
         norb.takeDamage(8);
-        std::cout << norb << std::endl;
         norb.attack("Dominic");
         norb.highFivesGuys();
     }
