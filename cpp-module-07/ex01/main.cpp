d292ef14c54f70f0f74543439fb7a67e9a537289
Italo A <italoaurelior@gmail.com>
2022-11-05 19:06:24 -0300

    refactor: remove complicated syntax

--- cpp-module-07/ex01/main.cpp
+++ cpp-module-07/ex01/main.cpp
@@ -10,22 +10,22 @@ int main(void)
 {
     {
         int array[] = { 0, 1, 2, 3, 4 };
-        ::iter(array, 5, print);
+        iter(array, 5, print);
     }
     std::cout << std::endl;
     {
         char array[] = { 'a', 'b', 'c', 'd', 'e' };
-        ::iter(array, 5, print);
+        iter(array, 5, print);
     }
     std::cout << std::endl;
     {
         std::string array[] = { "Hello", "World", "!" };
-        ::iter(array, 3, print);
+        iter(array, 3, print);
     }
     std::cout << std::endl;
     {
         float array[] = { 0.0f, 1.1f, 2.2f, 3.3f, 4.4f };
-        ::iter(array, 5, print);
+        iter(array, 5, print);
     }
 
     return 0;
